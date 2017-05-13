2b68f83fa8fd88b0552183b0119054d0c8f75b79
Sam Collet <scollet@student.42.us.org>
2017-05-13 15:40:10 -0700

    Update and rename object.js to screen-component.js

---
+++ js/screen-component.js
@@ -0,0 +1,12 @@
+AFRAME.registerComponent('screen', {
+  schema: {
+    color: {default: '#FFF'},
+    size: {type: 'int', default: 5}
+  },
+  init: function () {},
+  update: function () {},
+  tick: function () {},
+  remove: function () {},
+  pause: function () {},
+  play: function () {}
+});
